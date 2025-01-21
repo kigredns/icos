29110cd0f150ddb87db81dc418d9553beaf5d62a
SanderDevMaybe <mikolajkim585@gmail.com>
2025-01-22 00:36:40 +0100

    Update skibidisigma.lua

--- skibidisigma.lua
+++ skibidisigma.lua
@@ -1,71 +1,70 @@
 local player = game.Players.LocalPlayer
-
--- Zaktualizowana whitelist w formie zestawu (set)
+-- Zaktualizowana whitelist z dodatkowymi nickami
 local whitelist = {
-    ["wojtes_bmw"] = true, ["kajaaja8"] = true, ["davikaof2"] = true, ["fortesyt"] = true,
-    ["xxxahjmrkim"] = true, ["roma22aaa"] = true, ["freaknotthis"] = true, ["apullahh"] = true,
-    ["agamedveloperr"] = true, ["archie_lovesroblox73"] = true, ["ashwingamernov062013"] = true,
-    ["xdemon_zuko"] = true, ["jake727why"] = true, ["tuber93333666"] = true, ["omenino_hg"] = true,
-    ["joelex16mp"] = true, ["kirarija"] = true, ["mahaz1122"] = true, ["artthecut"] = true,
-    ["f_ajgn"] = true, ["thegemeur776"] = true, ["hewinzx"] = true, ["turyhd666"] = true,
-    ["criptical_bcc"] = true, ["kfgdhdhdhd"] = true, ["lukas_hack05"] = true, ["water9165"] = true,
-    ["zayo_alfa1"] = true, ["xxitalydarck91xx"] = true, ["devopgamer11"] = true, ["andreaxsolara"] = true,
-    ["dancgo8ud"] = true, ["ciqstek"] = true, ["guh2405rei"] = true, ["rtrt967"] = true,
-    ["madhya78"] = true, ["15dayslater4554"] = true, ["pitler12"] = true, ["hackandres_dreamxd"] = true,
-    ["gukzinh3ll"] = true, ["coucou_302"] = true, ["derareaz"] = true, ["v8bit2"] = true,
-    ["jams1119"] = true, ["rubinium0"] = true, ["pro_player2895"] = true, ["ggyfic68_764"] = true,
-    ["lacipropro"] = true, ["digitosim973"] = true, ["bkak_444"] = true, ["kkwildone"] = true,
-    ["superloulou60"] = true, ["1x1xroblox1x"] = true, ["holichyl"] = true, ["saitama112323"] = true,
-    ["o2s1u2rr2"] = true, ["samexsamex"] = true, ["rami000010"] = true, ["majestic371"] = true,
-    ["zrvll_1"] = true, ["okvgn_dontban"] = true, ["2115hack2115"] = true, ["kimsagaming"] = true,
-    ["aaapakdiddj"] = true, ["leoxplosionbenedetty"] = true, ["kiroosofinee"] = true,
-    ["ivannchava21"] = true, ["lacipropro0"] = true, ["olidragon210"] = true, ["sreymey2k"] = true,
-    ["kevingarcia1112"] = true, ["denstar123456"] = true, ["bro185"] = true, ["00x_alii"] = true,
-    ["aaapakdjdid"] = true, ["muhamadarif2013"] = true, ["indonesia_1279"] = true, ["13lil1ian"] = true,
-    ["sybau66"] = true, ["bladeballmm22"] = true, ["niskajy12300"] = true, ["matti_remastered"] = true,
-    ["caughtulacking14"] = true, ["678910he"] = true, ["winler07"] = true, ["leangip"] = true,
-    ["hanumang2021"] = true, ["kittysnowball35"] = true, ["dzifxicyxy"] = true, ["whatisawoman0"] = true,
-    ["lilbrostoleyogirl"] = true, ["normalbaconha6"] = true, ["fotpos"] = true, ["susychat_1"] = true,
-    ["meydsonlindoyt"] = true, ["candy_oc2023"] = true, ["alien89470"] = true, ["lucasmecher101"] = true,
-    ["deltaexecutorlink5"] = true, ["weber84430"] = true, ["antekwoszszczczek"] = true, ["azkahattaa"] = true,
-    ["currentaka"] = true, ["nolimitegoyjunior"] = true, ["valtttteri"] = true, ["itthegoodgamer5484"] = true,
-    ["imthegoatcr7sui_7"] = true, ["ezbydario"] = true, ["seb_xdb0"] = true, ["x3zlp"] = true,
-    ["dory1238bo"] = true, ["lilman073016"] = true, ["mskrnebd"] = true, ["ytplayz376"] = true,
-    ["pluhh32"] = true, ["elo_alanowiecz"] = true, ["imjustsusing"] = true, ["reyvings"] = true,
-    ["lotofsand"] = true, ["melikp001"] = true, ["asmrshade12345"] = true, ["alucardheroherobar2"] = true,
-    ["alexbutonmobile"] = true, ["daxyonyt"] = true, ["yoyo666687"] = true, ["also_nottze1"] = true,
-    ["ian2014_m"] = true, ["v3rticalslabs"] = true, ["joey02222016"] = true, ["pun123nool2"] = true,
-    ["keahidagod768"] = true, ["caaaa_308"] = true, ["sr_tocino353"] = true, ["jorge_62671"] = true,
-    ["d4ry4n"] = true, ["devilkitty_exe"] = true, ["gajkskaksk"] = true, ["krvinja"] = true,
-    ["dyspotbj"] = true, ["alexislebo"] = true, ["andri_hack908"] = true, ["animelordddddd"] = true,
-    ["kalelyt1233"] = true, ["roboduderkranto_o"] = true, ["bottomlessgluttony"] = true,
-    ["toria_1402"] = true, ["vincelawrencefromv"] = true, ["yoyo666687"] = true,
-    ["asmrshade12345"] = true, ["rose_love1458"] = true, ["oksanayel"] = true, ["ameerkingx"] = true,
-    ["dhutaob"] = true, ["vicki_nice9"] = true, ["rickdarky"] = true, ["kakaz1t"] = true,
-    ["syncix9"] = true, ["rayanletueur"] = true, ["noam5902"] = true, ["youki2021k"] = true,
-    ["luckyman_39"] = true, ["aladeath1"] = true, ["alhorijrwahabjr"] = true, ["munnaindia"] = true,
-    ["aizerion"] = true, ["pulmax14"] = true, ["janek0599"] = true, ["c00lkidd4661"] = true,
-    ["rodrigo_br157"] = true, ["9876543210hdud"] = true, ["star52050"] = true,
-    ["biely2kkj"] = true, ["2324vip1"] = true, ["vak_3zz"] = true, ["noob_066613"] = true,
-    ["maxence28000"] = true, ["littlemen_soprakenny"] = true, ["elpro97838"] = true, ["gogo_wee07"] = true,
-    ["hii71020"] = true, ["mylesplaystation"] = true, ["wicia6977"] = true,
-    ["headhittaa"] = true, ["ban55562"] = true, ["aaron200158"] = true, ["5zym0n456"] = true, ["kwmern"] = true
+"Wojtes_BMW", "KajaAja8", "Davikaof2", "FORTESYT", "Xxxahjmrkim", "roma22aaa", "freaknotthis",
+    "apullahh", "AGameDeveloperr", "ARCHIE_LOVESROBLOX73", "ashwingamernov062013", "xdemon_zuko", "JAKE727why",
+    "Tuber93333666", "Omenino_HG", "JOELEX16MP", "kirarija", "Mahaz1122", "ArtTheCut", "F_Ajgn", "thegemeur776",
+    "HeWinzx", "turyhd666", "CRIPTICAL_BCC", "kfgdhdhdhd", "lukas_hack05", "water9165", "Zayo_alfa1", "Xxitalydarck91xX",
+    "DEVOPGAMER11", "AndreaxSolara", "Dancgo8ud", "ciqstek", "guh2405rei", "rtrt967", "Madhya78", "15dayslater4554",
+    "Pitler12", "HackAndres_dreamXD", "gukzinh3ll", "coucou_302", "derareaz", "V8BIT2", "jams1119",
+    "Rubinium0", "Pro_player2895", "ggyfic68_764", "lacipropro", "DigitoSiM973", "BKAK_444", "kkwildone", "SuperLoulou60",
+    "1x1xroblox1x", "holichyl", "saitama112323", "o2s1u2rr2", "samexsamex", "rami000010", "Majestic371",
+    "Zrvll_1", "okvgn_DontBan", "2115Hack2115", "KimsaGaming", "aaapakdiddj", "leoxplosionbenedetty",
+    "KirooSoFinee", "IvannChava21", "lacipropro0", "olidragon210", "sreymey2k", "kevingarcia1112",
+    "denstar123456", "bro185", "00x_Alii", "aaapakdjdid", "muhamadarif2013", "indonesia_1279", "13lil1ian", "sybau66",
+    "bladeballmm22", "NISKAJY12300", "Matti_remastered", "caughtulacking14", "678910he", "winler07",
+    "Leangip", "Hanumang2021", "kittysnowball35", "dzifxicyxy", "Whatisawoman0", "Lilbrostoleyogirl",
+    "Normalbaconha6", "fotpos", "susychat_1", "MEYDSONLINDOyt", "candy_oc2023", "alien89470", "LucaSmecher101",
+    "Deltaexecutorlink5", "Weber84430", "AntekWoszszczczek", "azkahattaa", "CURRENTAKA",
+    "nolimitegoyjunior", "Valtttteri", "ItTheGoodGamer5484", "Imthegoatcr7sui_7", "ezbydario",
+    "seb_xdb0", "x3zlp", "Dory1238bo", "lilman073016", "mskrnebd", "YTplayz376", "pluhh32",
+    "ELO_ALANOWIECZ", "imjustsusing", "REYVINGS", "LotOfSand", "MelikP001", "Asmrshade12345", "alucardheroherobar2",
+    "alexbutonmobile", "DaXyOnYT", "yoyo666687", "coucou_302", "Also_NotTze1", "ian2014_m", "V3rticalSlabs",
+    "joey02222016", "Pun123nool2", "keahidagod768", "caaaa_308", "sr_tocino353", "jorge_62671",
+    "D4ry4n", "devilkitty_exe", "gajkskaksk", "krvinja", "DyspoTbj", "alexislebo", "Andri_Hack908", "ANIMELORDDDDDD",
+    "kalelyt1233", "RoboDudeRkranto_o", "BottomlessGluttony", "toria_1402", "vincelawrencefromv", "Yoyo666687",
+    "AsmrShade12345", "Rose_love1458", "oksanayel", "AmeerKiNGx", "dhutaob", "vicki_nice9", "RickDarKy", "kakaz1t",
+    "kakazit01naoe02", "Syncix9", "RAYANLETUEUR", "vicki_nice9", "pamilargabriel", "noam5902", "youki2021k",
+    "luckyMan_39", "AvaDeath1", "ALHORIJRWAHABjr", "Munnaindia", "Aizerion",
+    "PULMAX14", "janek0599", "c00lkidd4661", "Rodrigo_BR157", "9876543210hdud", "star52050", "biely2kkj",
+    "2324VIP1", "VAK_3ZZ", "noob_066613", "maxence28000", "littleMEN_sopraKENNY", "elpro97838", "gogo_wee07",
+    "hii71020", "MylesPlayStation", "SuperLoulou60", "Kwmern", "Erick_gamer710", "furtzge",
+    "Jqjshzhaj", "aomgwwe113", "wazawazaaa7", "PinkSunSmoke", "x1GORxRAFAELx", "PainThatAlters1",
+    "PainThatAlters1", "timbavk_d", "kyoslegacy", "Perroalt98", "Ali352400", "Dj_Sizle", "tribu3zi", "Aizerion",
+    "0524Marty", "Elgato0158", "Rayane187332", "lamatonwm", "ZXArturoZX123", "freemikey001", "XxJuanxdxXMTM",
+    "Nestorgolybev201", "Berlinerali", "mamadmmad997", "ggandria11", "6kxlisupre", "daxf125",  
+"AngelosMod", "ophub000", "thiagonoobbr1", "yAZiUU6071", "Yourweird693", "portuga_c4",  
+"gcfrfhdjx2account", "ytr_Jaozin", "drfantasy91", "JuhSFDAH", "Angelofrancisco7", "kitti200300",  
+"mahfooz45", "ILoveYou_Alana2", "IloveyouTanq", "FarmerManatee", "DorukAliCelik90", "Fable_Cable",  
+"GOON1380", "Ingoul23", "Xanesiy", "Zrk974ii", "thegenier", "Beicon_daquebrada",  
+"lehadro_greek15", "ghjghjkfju", "yaboiaction9012", "ta1luar", "dreckpro143", "furtzge",
+"Araxis2223", "WhosTrubs", "iiiiiiiiiignas", "itzbaba2", "GLITCHEDSMG44", "forma266", "3omda4reall",
+"jerfie22", "spidermanboy11900", "gamersroblox5D", "AgentR6Retro", "yamir19385", "Oscarmanguia",
+"gagalol2", "Berlinerali", "Ban55562", "only_Acqua", "TNT_COOKIE35", "Real_Bloodhoodlife", "jolataue3", "Gady1203_2",
+"ajfhlka538", "Bro_567452", "tanqrrrrfannnnn1", "Imwibukun", "Krutoichel769", "XxBarackGamingzLOL", "Voner956", "createchgamimg",
+"aqwariy", "megaeddie759", "KADZUTORAN1", "Hyexgamer3550", "Roman304010", "noobyguy_1009", "Gamer222boom", "xALOESx21", "Emrelol43", "AceK1nd",
+"victor134lko", "dana_Uwy", "luckmioginho", "hdugpfsugs6r", "Gady1203_2", "sweet_tarts738", "edduard647", "risottoJr", "Hanaw1123", "Noetarbler",
+"LorenzoSantos138", "raito2574","y2kpHoOx09p", "megaeddie759" 
 }
-
--- Funkcja normalizująca nazwę użytkownika (zmiana na małe litery i usunięcie spacji)
+-- Funkcja normalizująca nazwę użytkownika (np. do małych liter)
 local function normalizeUsername(username)
-    return username:lower():gsub("%s+", "")
+    return username:lower():gsub("%s+", "")  -- Zmienia na małe litery i usuwa spacje
 end
-
 -- Funkcja sprawdzająca, czy użytkownik jest na whiteliście
 local function isWhitelisted(username)
-    return whitelist[normalizeUsername(username)] or false
+    local normalizedName = normalizeUsername(username)
+    for _, whitelistedName in ipairs(whitelist) do
+        if normalizeUsername(whitelistedName) == normalizedName then
+            return true
+        end
+    end
+    return false
 end
-
--- Sprawdzenie użytkownika i reakcja na wynik
+-- Sprawdzenie i reakcja na wynik
 if isWhitelisted(player.Name) then
     print("Whitelisted: " .. player.Name)
 else
+    -- Powiadomienie i wyrzucenie użytkownika
     print(player.Name .. " is not on the whitelist. Kicking...")
-    player:Kick("Hello, to use Vip you need to buy a shirt for 65 Robux")
+    player:Kick("You are not on the whitelist. Please contact support for assistance.")
 end

81f69c94bad16c9c2e7285a08f78390d41d34e2d
SanderDevMaybe <mikolajkim585@gmail.com>
2025-01-24 21:06:44 +0100

    Update skibidisigma.lua

--- skibidisigma.lua
+++ skibidisigma.lua
@@ -44,7 +44,7 @@ local whitelist = {
 "ajfhlka538", "Bro_567452", "tanqrrrrfannnnn1", "Imwibukun", "Krutoichel769", "XxBarackGamingzLOL", "Voner956", "createchgamimg",
 "aqwariy", "megaeddie759", "KADZUTORAN1", "Hyexgamer3550", "Roman304010", "noobyguy_1009", "Gamer222boom", "xALOESx21", "Emrelol43", "AceK1nd",
 "victor134lko", "dana_Uwy", "luckmioginho", "hdugpfsugs6r", "Gady1203_2", "sweet_tarts738", "edduard647", "risottoJr", "Hanaw1123", "Noetarbler",
-"LorenzoSantos138", "raito2574", "y2kpHoOx09p", "5zym0n456", "JakeIsPail",
+"LorenzoSantos138", "raito2574", "y2kpHoOx09p", "5zym0n456", "JakeIsPail", "Rownokboss",
 "KJ_911404",  "Pro_241656512", "miguel_pvp441", "SZYMONKONIECZN", "kyleakdarl", "JakeIsPail", "RobKLPC", "todikorik", "Morda_Off",
 "mariomaro9089098", "GUYIDBIN", "Aaron200158", "headhittaa", "Monkey_4you2", "luthfi123456l", "megaeddie759" 
 }

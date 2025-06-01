caa02dd8ec05c47b6d79173879afe2b3b2101f19
SanderDevMaybe <mikolajkim585@gmail.com>
2025-06-01 13:54:05 +0200

    Update skibidisigma.lua

--- skibidisigma.lua
+++ skibidisigma.lua
@@ -73,8 +73,8 @@ local whitelist = {
       "renegado_777899", "Nikos55809", "king_god3231yt", "Chara_1570", "jighg8ugtn78t", "leongardyan", "amHJKaAkheE",
      "XCreative_ArasX", "sayede127", "aswath_void", "Carterade_trabaio", "Kikyoinlove", "CandyCorn_316", "ShadowsOfKindom", "superrdy_g79010", "epic_0111", "Pedrinh0_cr4ft157",
      "randomastroid123456", "PaulFreund", "BrunoHenrique1935", "manvik09830", "CiaoNonSono_Normale", "k5d6r", "TheLord_Ege", "Raw191os", "sud0_o", "Kayah11102", "I_SPY1736",
-     "summermuffin990", "fadhilzayyani", "proplayer800C", "pinkulpieski"
-
+     "summermuffin990", "fadhilzayyani", "proplayer800C", "pinkulpieski", "WolfPaq_OFC00", "Noob_837r7747", "wnjindiujwujdawwad", "mbappeezo", "sebastiensapio123", "Arikitaylor07",
+     "zcghhhhtt", "AdorableTootPug", "slavs25400"
 
 }
 
